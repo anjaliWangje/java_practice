d3c13e87072408e8fab6e8d260472aa2996c852f
anjaliWangje <anjalieknathwangje@gmail.com>
2024-09-06 22:19:55 +0530

    Add files via upload

---
+++ abstractclass1.java
@@ -0,0 +1,34 @@
+package abstaction;
+
+ abstract public class abstractclass1 {
+     abstract void show();
+
+
+     void display() {
+         System.out.println("this is display ");
+     }
+ }
+ abstract class abstraction234 extends abstractclass1{
+     abstract void testing();
+
+     }
+
+
+class abstractiontest123  {
+     void display23(){
+         System.out.println("this is display123");
+
+    }
+    void showing(){
+         System.out.println("this is showing");
+    }
+}
+
+
+
+
+
+
+
+
+
